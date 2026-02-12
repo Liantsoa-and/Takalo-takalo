c680586096fae0fc5242cada33788d4071cdbf9f
Liantsoa-and <liantsoaandreane@gmail.com>
2026-02-12 10:10:00 +0300

    fix : reglage de la taille des cartes faites pour mes objets

--- app/views/objet/liste.php
+++ app/views/objet/liste.php
@@ -2,7 +2,9 @@
 <?php $objets = $objets ?? []; ?>
 
 <main class="col-12 col-md-10 py-4">
-<?php $categories = $categories ?? []; $scope = 'mine'; include __DIR__ . '/search.php'; ?>
+    <?php $categories = $categories ?? [];
+    $scope = 'mine';
+    include __DIR__ . '/search.php'; ?>
 
     <div class="row mb-4">
         <div class="col-md-8">
@@ -20,16 +22,16 @@
             Vous n'avez pas encore d'objets. <a href="<?= $base ?>/objet/formulaire">Créez votre premier objet</a>
         </div>
     <?php else: ?>
-        <div id="objects-list-mine" class="row objects-list">
+        <div id="objects-list-mine" class="objects-list">
             <?php include __DIR__ . '/_cards.php'; ?>
         </div>
     <?php endif; ?>
 </main>
 
 <script>
-function confirmDelete(id) {
-    if (confirm('Êtes-vous sûr de vouloir supprimer cet objet ?')) {
-        window.location.href = '<?= $base ?>/objet/' + id + '/delete';
+    function confirmDelete(id) {
+        if (confirm('Êtes-vous sûr de vouloir supprimer cet objet ?')) {
+            window.location.href = '<?= $base ?>/objet/' + id + '/delete';
+        }
     }
-}
 </script>
